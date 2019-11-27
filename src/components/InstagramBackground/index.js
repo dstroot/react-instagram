f9222755d666d73470f6b1cca67ef4df63b2ba9c
Dan Stroot <dan.stroot@gmail.com>
2019-11-26 17:15:20 -0800

    Update index.js

--- src/components/InstagramBackground/index.js
+++ src/components/InstagramBackground/index.js
@@ -8,12 +8,11 @@ import { fetchNetlify } from './queries';
 
 // https://gist.github.com/kjintroverted/d67c7f12f68288f6ccf07cbd06fa66a8
 
-export const InstagramBackground = ({ username, quality }) => {
+export const InstagramBackground = ({ username }) => {
   const [images, setImages] = useState(null);
   const [imageDims, setImageDims] = useState(0);
-  // const [quality, setQuality] = useState(1);
+  const [quality, setQuality] = useState(1);
   const { data, isLoading, error } = useQuery(
-    //['photos', { username, quality }],
     ['photos', { username }],
     fetchNetlify,
     {
@@ -40,29 +39,30 @@ export const InstagramBackground = ({ username, quality }) => {
     if (!images || !images.length) {
       setImageDims(0);
     } else {
-      setImageDims(
-        Math.floor(
+      let dimension = Math.floor(
           Math.sqrt((window.outerHeight * window.outerWidth) / images.length)
-        )
       );
+      setImageDims(dimension);
+//       calcQuality(dimension);
     }
   }
-  
-// function calcQuality(imageDims) {
-//   switch(imageDims) {
-//   case (imageDims < 150):
+
+  /** The quality of the images. Range is 0-4. 0 = 150x150, 1 = 240x240, 2 = 320x320, 3 = 480x480, 4 = 640x640. Defaults to 1 */
+// function calcQuality(dimension) {
+//   switch(dimension) {
+//   case (dimension < 150):
 //     setQuality(0)
 //     break;
-//   case (imageDims < 240):
+//   case (dimension < 240):
 //     setQuality(1)
 //     break;
-//   case (imageDims < 320):
+//   case (dimension < 320):
 //     setQuality(2)
 //     break;
-//   case (imageDims < 480):
+//   case (dimension < 480):
 //     setQuality(3)
 //     break;
-//   case (imageDims < 640):
+//   case (dimension < 640):
 //     setQuality(4)
 //     break;
 //   default:
@@ -113,7 +113,7 @@ export const InstagramBackground = ({ username, quality }) => {
         {images &&
           images.map(res => (
             <Tile key={res[0].src}>
-              <Post src={res[quality || 1].src} alt="recent post" />
+              <Post src={res[quality].src} alt="recent post" />
             </Tile>
           ))}
       </Container>
@@ -125,12 +125,7 @@ export const InstagramBackground = ({ username, quality }) => {
 
 InstagramBackground.propTypes = {
   /** The username of the Instagram account to use  */
-  username: PropTypes.string.isRequired,
-  /** The quality of the images. Range is 0-4. 0 = 150x150, 1 = 240x240, 2 = 320x320, 3 = 480x480, 4 = 640x640. Defaults to 1 */
-  quality: PropTypes.number,
+  username: PropTypes.string.isRequired
 };
 
-// Specifies the default values for props:
-InstagramBackground.defaultProps = {
-  quality: 1,
-};
+
