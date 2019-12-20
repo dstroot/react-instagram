32382c8bb0ace1905cc4c702162726709f32cfcd
Dan Stroot <dan.stroot@gmail.com>
2019-12-20 06:59:55 -0800

    refactor form

--- src/components/InstaForm/index.js
+++ src/components/InstaForm/index.js
@@ -11,8 +11,28 @@ export const InstaForm = ({ value, handler }) => {
   };
 
   const handleChange = event => {
-    setInput(event.target.value);
+    // input validation
+    if (event.target.value.length <= 30) {
+      setInput(event.target.value);
+    }
   };
+
+  // Regedit to match field
+  // ^([\w.-](?!\.(com|net|html?|js|jpe?g|png)$)){5,}$
+
+  // ^              # from start
+  // ([\w.-]        # \w is equal to [a-zA-Z0-9_]
+  //     (?!\.          # in front can NOT be a dot followed by
+  //         (com           # com
+  //         |net           # OR net
+  //         |html?         # OR htm or html     # ? means optional match
+  //         |js            # OR js
+  //         |jpe?g         # OR jpg or jpeg
+  //         |png           # OR png
+  //         )$             # block only if it is at the end
+  //     )              # end of the negative lookahead
+  // ){5,}          # match at least 5 characters in above conditions
+  // $              # till the end
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -43,21 +63,22 @@ const Input = styled.input`
   color: white;
   background-color: transparent;
   font-size: 1.1em;
-  padding: 1em 2em;
+  padding: 1em;
   border: 2px solid white;
   border-radius: 3px;
   margin-top: 10px;
   margin-right: 10px;
-  width: 185px;
+  min-width: 300px;
 `;
 
 const WhiteButton = styled.button`
-  background-color: White;
+  background-color: white;
   /* let background bleed through for text */
-  mix-blend-mode: lighten
+  mix-blend-mode: lighten;
+  color: blue;
   font-weight: 600;
   font-size: 1.1em;
-  padding: 1em 3em;
+  padding: 1em;
   border: 2px solid white;
   border-radius: 3px;
   margin-top: 10px;
