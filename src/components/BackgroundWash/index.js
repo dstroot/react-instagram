8de071e3740f44f8dfed1e79164386a81f48b4ce
Dan Stroot <dan.stroot@gmail.com>
2020-09-08 06:42:27 -0700

    animated wash

--- src/components/BackgroundWash/index.js
+++ src/components/BackgroundWash/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 
 export const BackgroundWash = ({ filterOpts = [] }) => {
-  // creates a color "wash" over the page
+  // creates an animated color "wash" over the page
+  const backgroundAnimation = keyframes`
+    0% { background-position: 25% 0%; }
+    50% { background-position: 76% 100%; }
+    100% { background-position: 25% 0%; }
+  `;
+
   const Filter = styled.div`
     position: fixed;
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(${filterOpts.join()});
     opacity: 0.6;
+    background-size: 400% 400%;
+    animation: ${backgroundAnimation} 10s ease infinite;
   `;
 
   return <Filter />;
